0488d1659244373d71ae5973be5f7e563d76b4fa
Shaik Shaguftha Ahmed <shaikshagu12@gmail.com>
2026-06-28 10:07:12 +0530

    Time: 5 ms (79.58%), Space: 46.3 MB (6.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+    int rev=0;
+     int digit=0;
+      while(x>0){
+        
+         digit=x%10;
+          rev=(rev*10)+digit;
+           x=x/10; 
+           }
+        if(rev==temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
