6b80030bb2bd49c1719bfd2c36455dc2b73a6ff9
Shaik Shaguftha Ahmed <shaikshagu12@gmail.com>
2026-06-28 11:11:34 +0530

    Time: 0 ms (100%), Space: 45.1 MB (27.19%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                return true;
+            }
+            if(arr[low]==arr[mid] && arr[high]==arr[mid]){
+                low++;
+                high--;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && arr[mid]>target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(arr[mid]<target && arr[high]>=target){
+                    low=mid+1;
+
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+return false;
+    }
+}
