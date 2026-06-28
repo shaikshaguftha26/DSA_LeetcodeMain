a1c9c171e7b767e4612915db32132dc087167998
Shaik Shaguftha Ahmed <shaikshagu12@gmail.com>
2026-06-28 10:09:49 +0530

    Time: 2465 ms (5.01%), Space: 42.4 MB (5.45%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(long i=1;i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
+          
+        }
+          return false;
+
+        
+    }
+}
