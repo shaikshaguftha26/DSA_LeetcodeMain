6c8f37a9fb1991e158c255d9028744c1257afa61
Shaik Shaguftha Ahmed <shaikshagu12@gmail.com>
2026-08-23 13:50:56 +0530

    Time: 1 ms (61.47%), Space: 43 MB (84.72%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -6,7 +6,7 @@ class Solution {
             set1.add(jewels.charAt(i));
         }
         int count=0;
-        HashSet<Character> set2=new HashSet<>();
+        
         for(int i=0;i<stones.length();i++){
             if(set1.contains(stones.charAt(i))){
                 count++;
