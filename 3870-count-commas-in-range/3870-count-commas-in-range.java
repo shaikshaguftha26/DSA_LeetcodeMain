54d95581228da4577d1a6b6c2b5df2537fe88787
Shaik Shaguftha Ahmed <shaikshagu12@gmail.com>
2026-09-08 21:23:05 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,8 +1,9 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000){
-            return 0;
-        }
-        return n-1000+1;
+        // if(n<1000){
+        //     return 0;
+        // }
+        // return n-1000+1;
+        return Math.max(n-999,0);
     }
 }
